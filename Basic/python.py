3a5d6d9eb2f78ec69d8c4d65f47c6f8ed04eb9a8
faryadazim <faryadmaalic92@yahoo.com>
2024-01-25 03:19:54 +0500

    variables

--- Basic/python.py
+++ Basic/python.py
@@ -6,9 +6,20 @@
 # Variable 
 fruit = "mango"
 numbs = 1
+numbs = "lorem"
+
+print(fruit, numbs)
 
 
-print("============" , fruit, numbs)
+# x,y,z =1,2,"lorem"
+x,y,z =(1,2,"lorem")
+
+print(x,y,z)
 
 
+# casting 
+print(type(x)) 
+
+x = str(x)
+print(type(x)) 
 
